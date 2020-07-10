c533d7f8db9598c691d03289943a493b66f10f8a
Fathu Danur <dfathu@gmail.com>
2020-07-10 18:21:49 +0700

    Updates

---
+++ precache-manifest.80ddfb0b95192244cfb046985f7969e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2825949ae625fb89e17642d505a62f0",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "dc8a5f4048648cf64451",
+    "url": "/expense-tracker/static/css/main.3aab3814.chunk.css"
+  },
+  {
+    "revision": "6d863c77ddf3fbc5c646",
+    "url": "/expense-tracker/static/js/2.96ad2364.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/expense-tracker/static/js/2.96ad2364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8a5f4048648cf64451",
+    "url": "/expense-tracker/static/js/main.0ebbb0ee.chunk.js"
+  },
+  {
+    "revision": "36eb252feb7be374ca54",
+    "url": "/expense-tracker/static/js/runtime-main.80004107.js"
+  }
+]);
